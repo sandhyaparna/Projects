89ccdfb93216ca7d2abfaadde8eb87d2c516182b
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-08-06 11:46:47 -0500

    Update and rename Comparison of NLP Models.py to Comparison of NLP Pre-Trained Models.py

--- Comparison of NLP Models.py
+++
@@ -1,4 +0,0 @@
-# ELMo, USE, BERT, XLNet
-
-# Sentence Similarity - Identify if one sentence is similar to another
-
---
+++ Comparison of NLP Pre-Trained Models.py
@@ -0,0 +1,11 @@
+# ELMo, USE, BERT, XLNet
+
+# Sentence Similarity - Identify if one sentence is similar to another
+# Colab Notebook - https://colab.research.google.com/drive/1d_bJqe6x7WM1_50VX0rOPKuOLbhCyo39
+
+
+
+
+
+
+
